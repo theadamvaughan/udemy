2478aadcd687b588981ad070251fe38fe96b79f0
Adam Vaughan <adam_vaughan1982@hotmail.com>
2019-03-16 18:55:38 +0000

    early lesson on Ruby

---
+++ hello.rb
@@ -0,0 +1,100 @@
+# NAME = 'Adam'
+# last_name = 'Vaughan'
+# handsome = true
+# age = 36
+
+# # puts NAME + ' ' + last_name
+
+# # p last_name.upcase
+
+# # p last_name.inspect
+
+# p "Hello #{NAME}, how are you?"
+
+# p "My name is #{NAME} #{last_name} and I am #{age} years old"
+
+# puts "can I have your name"
+# name = gets.chomp
+
+# # puts "thank you for giving me your name, #{name}"
+
+# str = "5"
+# p str
+# p str.class
+# # p str.to_f
+
+# # p 5.to_f == 5.0
+
+# a = 3
+
+# 10.times do |i|
+#   b = (i + 1) 
+#   puts a * b
+# end
+
+
+# 10.times { |i| puts 3 * (i + 1) }
+
+# 10.downto(0) { |i| puts "Thunderbird #{i}" }
+
+# 99.downto(1) do |num|
+#   puts "#{num} bottles of beer on, #{num} bottles of beer"
+#   b = (num - 1)
+#   puts "you take one down and pass it around, #{b} bottles of beer on the wall\n "
+# end
+
+# 0.step(100, 5) { |num| puts num }
+
+# name = String.new("hello world")
+# puts name
+
+# words = <<MLS
+# Hello 
+# my
+# name
+# is
+# Adam
+# MLS
+
+# puts words
+
+# puts 'Juliet said \'goodbye\' to Romeo'
+
+# a = "Hello world"
+# b = "My name is Adam"
+# c = " "
+# d = ""
+
+# p a.length.even?
+# p b.length
+# p c.length
+# p d.length
+
+# puts
+
+# p a.size
+# p b.size
+# p c.size
+# p d.size
+
+# story = "Once upon a time in a land far, far away"
+
+# # p story.length
+
+# # p story[0, story.length]
+
+# p story[27..39]
+# p story[27...390]
+
+# fact = "I love blueberry pie"
+# p fact
+
+# fact[7, 4] = "rasp"
+# p fact
+
+# fact[2..5] = "hate"
+# p fact
+
+word = "hello"
+p word
+p word.capitalize!
