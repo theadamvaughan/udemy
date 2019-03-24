2c53aa2c88cd51797ce7d1a6f724651cb8ea8eaf
Adam Vaughan <adam_vaughan1982@hotmail.com>
2019-03-24 16:36:35 +0000

    added code that asks for the maximum ABV you want in a beer and returns those all equal to or less than the users input

--- beer_bottles.rb
+++ beer_bottles.rb
@@ -30,6 +30,15 @@ drinks = [
 },
 
 {
+  name: "Tennants Super",
+  container: "can",
+  volume: 9,
+  amount_drunk: 10,
+  description: "Odd taste. Popular drink to get drunk quickly on a budget",
+  alcoholic: true
+},
+
+{
   name: "Hop House",
   container: "bottle",
   volume: 5,
@@ -40,28 +49,44 @@ drinks = [
 
 # Returns all the alccoholic drinks
 
-def alcoholic(drink)
-  drink.each do |hash|
-    if hash[:alcoholic]
-    puts "#{hash[:name]}"
-    end
-  end
-end
+# def alcoholic(drink)
+#   drink.each do |hash|
+#     if hash[:alcoholic]
+#     puts "#{hash[:name]}"
+#     end
+#   end
+# end
 
-alcoholic(drinks)
+# alcoholic(drinks)
 
 # Returns all the drink details
 
-def details(arr)
-  arr.each do |arr|
-    puts "#{arr[:name]} comes in a #{arr[:container]}. So far #{arr[:amount_drunk]}% of the drink has been drunk. Critics have described it as; '#{arr[:description]}'."
-    if arr[:alcoholic]
-      puts "This drink is alcoholic, The alcohol percentage is #{arr[:volume]}%"
-    elsif 
-      puts "You can drive after drinking this drink as the alcohol percentage is #{arr[:volume]}%"
+# def details(arr)
+#   arr.each do |arr|
+#     puts "#{arr[:name]} comes in a #{arr[:container]}. So far #{arr[:amount_drunk]}% of the drink has been drunk. Critics have described it as; '#{arr[:description]}'."
+#     if arr[:alcoholic]
+#       puts "This drink is alcoholic, The alcohol percentage is #{arr[:volume]}%"
+#     elsif 
+#       puts "You can drive after drinking this drink as the alcohol percentage is #{arr[:volume]}%"
+#     end
+#     puts
+#   end
+# end
+
+# details(drinks)
+
+
+
+
+def percentage(drink)
+  puts "What is the maximum ABV that you are after?"
+  input = gets.chomp.to_i
+  drink.each do |hash|
+    volume = hash[:volume].to_i
+    if volume <= input
+    puts "#{hash[:name]} - #{hash[:volume]}%"
     end
-    puts
   end
 end
 
-details(drinks)
+percentage(drinks)
