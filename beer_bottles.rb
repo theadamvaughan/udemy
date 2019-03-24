e5f4ecf88e9883db5d53a1abfe5d5463295f4845
Adam Vaughan <adam_vaughan1982@hotmail.com>
2019-03-24 15:21:15 +0000

    updated the method that returns all the drink information. made the alcoholic percentage an interger

--- beer_bottles.rb
+++ beer_bottles.rb
@@ -5,32 +5,43 @@ drinks = [
   {
   name: "Carlsberg",
   container: "bottle",
-  volume: "5%",
+  volume: 4,
   amount_drunk: 25,
   description: "taste's like piss. cheap to buy",
-  alcoholic: true 
+  alcoholic: true
 },
 
 {
   name: "Becks Blue",
   container: "bottle",
-  volume: "0%",
+  volume: 0,
   amount_drunk: 25,
   description: "Not bad for a non-alcoholic drink although it takes getting used to",
   alcoholic: false
 },
 
 {
+  name: "San Miguel",
+  container: "can",
+  volume: 5,
+  amount_drunk: 75,
+  description: "Nice medium strength beer",
+  alcoholic: true
+},
+
+{
   name: "Hop House",
   container: "bottle",
-  volume: "5%",
+  volume: 5,
   amount_drunk: 50,
   description: "Nice beer made by Guiness",
   alcoholic: true
 } ]
 
-def alcoholic(arr)
-  arr.each do |hash|
+# Returns all the alccoholic drinks
+
+def alcoholic(drink)
+  drink.each do |hash|
     if hash[:alcoholic]
     puts "#{hash[:name]}"
     end
@@ -39,17 +50,18 @@ end
 
 alcoholic(drinks)
 
-# def details(arr)
-#   puts "Name of the drinks is: #{arr[:name]}"
-#   puts "It comes in a #{arr[:container]}"
-#   puts "The alcohol percentage is #{arr[:volume]}"
-#   puts "So far #{arr[:amount_drunk]}% of the drink has been drunk"
-#   puts "Critics description: #{arr[:description]}"
-#   if arr[:alcoholic] == true
-#     puts "This drink is alcoholic"
-#   elsif 
-#     puts "You can drive after drinking this drink"
-#   end
-# end
+# Returns all the drink details
 
-# puts details(drink_2)
+def details(arr)
+  arr.each do |arr|
+    puts "#{arr[:name]} comes in a #{arr[:container]}. So far #{arr[:amount_drunk]}% of the drink has been drunk. Critics have described it as; '#{arr[:description]}'."
+    if arr[:alcoholic]
+      puts "This drink is alcoholic, The alcohol percentage is #{arr[:volume]}%"
+    elsif 
+      puts "You can drive after drinking this drink as the alcohol percentage is #{arr[:volume]}%"
+    end
+    puts
+  end
+end
+
+details(drinks)
