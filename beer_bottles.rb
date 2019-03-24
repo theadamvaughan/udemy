0595255d0ddfff6eb745bfc8b6128f6e2dadfe6f
Adam Vaughan <adam_vaughan1982@hotmail.com>
2019-03-24 17:39:50 +0000

    small updates to the code to make it flow

--- beer_bottles.rb
+++ beer_bottles.rb
@@ -47,42 +47,46 @@ drinks = [
   alcoholic: true
 } ]
 
-# Returns all the alccoholic drinks
+# .........................Returns all the drink details ...................
 
-# def alcoholic(drink)
-#   drink.each do |hash|
-#     if hash[:alcoholic]
-#     puts "#{hash[:name]}"
-#     end
-#   end
-# end
+puts "Here are all the drinks we have to offer;"
 
-# alcoholic(drinks)
+def details(arr)
+  arr.each do |arr|
+    puts "#{arr[:name]} comes in a #{arr[:container]}. So far #{arr[:amount_drunk]}% of the drink has been drunk. Critics have described it as; '#{arr[:description]}'."
+    if arr[:alcoholic]
+      puts "This drink is alcoholic, The alcohol percentage is #{arr[:volume]}%"
+    elsif 
+      puts "You can drive after drinking this drink as the alcohol percentage is #{arr[:volume]}%"
+    end
+    puts
+  end
+end
 
-# Returns all the drink details
+details(drinks)
 
-# def details(arr)
-#   arr.each do |arr|
-#     puts "#{arr[:name]} comes in a #{arr[:container]}. So far #{arr[:amount_drunk]}% of the drink has been drunk. Critics have described it as; '#{arr[:description]}'."
-#     if arr[:alcoholic]
-#       puts "This drink is alcoholic, The alcohol percentage is #{arr[:volume]}%"
-#     elsif 
-#       puts "You can drive after drinking this drink as the alcohol percentage is #{arr[:volume]}%"
-#     end
-#     puts
-#   end
-# end
+# .......................Returns all the alccoholic drinks...................
 
-# details(drinks)
+def alcoholic(drink)
+  puts "Would you like to see just the alcoholic drinks?"
+  answer = gets.chomp
+  puts "The following drinks are alcoholic;" if answer == "yes" 
+    drink.each do |hash|
+    puts "#{hash[:name]}"if hash[:alcoholic]
+  end
+end
 
+alcoholic(drinks)
 
 
+# .....................Returns drinks within a certain ABV range ...................
 
 def percentage(drink)
   puts "What is the minimum ABV that you are after?"
   low = gets.chomp.to_i
   puts "What is the maximum ABV that you are after?"
   high = gets.chomp.to_i
+  puts "The following drinks fall within your criteria;"
   drink.each do |hash|
     volume = hash[:volume].to_i
     puts "#{hash[:name]} - #{hash[:volume]}%" if (volume >= low) && (volume <= high)
